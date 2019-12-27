7ec110f80d7bd97c0347c34e098daca5c133e005
rtestard <rtestard@opensense.fr>
2019-12-27 16:59:48 +0100

    I am working a script to build component-index.js

---
+++ scripts/skindex.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
+var args = require('optimist').argv;
+var chokidar = require('chokidar');
+
+var componentIndex = path.join('component-index.js');
+var componentIndexTmp = componentIndex+".tmp";
+var componentsDir = path.join('src', 'components');
+var componentGlob = '**/*.js';
+var prevFiles = [];
+
+function reskindex() {
+    var files = glob.sync(componentGlob, {cwd: componentsDir}).sort();
+    console.log(files);
+    prevFiles = files;
+
+    for(file of files) {
+        const n = file.lastIndexOf('/');
+        console.log(n)
+    }
+
+    var strm = fs.createWriteStream(componentIndexTmp);
+
+    strm.write("/*\n");
+    strm.write(" * THIS FILE IS AUTO-GENERATED\n");
+    strm.write(" * You can edit it you like, but your changes will be overwritten,\n");
+    strm.write(" * so you'd just be trying to swim upstream like a salmon.\n");
+    strm.write(" * You are not a salmon.\n");
+    strm.write(" */\n\n");
+}
+
+reskindex();
